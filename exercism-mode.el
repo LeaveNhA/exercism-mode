9cf56e8977b0aabc4ff74b4f97961fdeb9b96e98
LeaveNhA <seckin.kukrer@hotmail.com>
2017-11-30 01:10:59 +0300

    last of 0.5

--- exercism-mode.el
+++ exercism-mode.el
@@ -2,7 +2,7 @@
 ;; Author     : Seçkin KÜKRER
 ;; Created    : November 2017
 ;; Modified   : 2017
-;; Version    : 0.0.4
+;; Version    : 0.0.5
 ;; Keywords   : exercism exercism.io exercism-mode
 ;; X-URL      : https://github.com/leavenha/exercism-mode
 ;; Last-saved : 2017-Nov-28
@@ -23,14 +23,28 @@
 ;; New implementation of `fetch` command in Exercism.
 ;; New language definations added.
 ;; Seperated functions for easy maintaining in the future.
+;; Works independent from file/buffer!
 ;; [0.0.3]
 ;; First public version.
 
 ;;; Code:
 
+(defun exercism-helper/if-s|n->s (sym)
+  (cond
+   ((stringp sym) (if (compare-strings
+                       (symbol-name (type-of sym))
+                       0 5
+                       "string"
+                       0 5)
+                      sym
+                    ""))
+   ((symbolp sym) "")))
+
 (defun exercism-mode/style-given-tracks (str)
   "highlight the language names!"
-  (replace-regexp-in-string "^[\s-]+\\(.\\)+[\s-]+" (lambda (s) s) str))
+  (replace-regexp-in-string "^[\s-]+\\(.+\\)[\s-]" (lambda (s) (symbol-name (type-of s))) str))
+
+(with-temp-buffer (exercism-mode/style-given-tracks (exercism-mode/call-shell-command "exercism tracks")))
 
 (defun exercism-mode/call-shell-command (command)
   "Run the given command in the Shell!"
@@ -44,37 +58,39 @@
 (defun exercism-mode/fetch-new-exercise ()
   "Fetch a new exercise from Exercism.io! This command fetch new exercise based on opened buffer-file type/`extension`."
   (interactive)
-  (let* ((language-name (pcase (file-name-extension (buffer-file-name))
-                         ("clj" "clojure")
-                         ("c" "c")
-                         ("cpp" "cpp")
-                         ("cs" "csharp")
-                         ("fs" "fsharp")
-                         ("cfml" "cfml")
-                         ("coffee" "coffeescript")
-                         ("lisp" "common-lisp")
-                         ("rpt" "crystal")
-                         ("d" "d")
-                         ("pas" "delphi")
-                         ("go" "go")
-                         ("ex" "elixir")
-                         ("exs" "elixir")
-                         ("elm" "elm")
-                         ("hs" "haskell")
-                         ("lhs" "haskell")
-                         ("js" "javascript")
-                         ("jl" "julia")
-                         ("kt" "kotlin")
-                         ("kts" "kotlin")
-                         ("erl" "erlang")
-                         ("lua" "lua")
-                         ("cljs" "clojurescript")
-                         ("java" "java")
-                         )))
+  (let* ((language-name (pcase (exercism-helper/if-s|n->s (file-name-extension (exercism-helper/if-s|n->s (buffer-file-name))))
+                          ("clj" "clojure")
+                          ("c" "c")
+                          ("cpp" "cpp")
+                          ("cs" "csharp")
+                          ("fs" "fsharp")
+                          ("cfml" "cfml")
+                          ("coffee" "coffeescript")
+                          ("lisp" "common-lisp")
+                          ("rpt" "crystal")
+                          ("d" "d")
+                          ("pas" "delphi")
+                          ("go" "go")
+                          ("ex" "elixir")
+                          ("exs" "elixir")
+                          ("elm" "elm")
+                          ("hs" "haskell")
+                          ("lhs" "haskell")
+                          ("js" "javascript")
+                          ("jl" "julia")
+                          ("kt" "kotlin")
+                          ("kts" "kotlin")
+                          ("erl" "erlang")
+                          ("lua" "lua")
+                          ("cljs" "clojurescript")
+                          ("java" "java")
+                          )))
     (if language-name
         (message (exercism-mode/call-shell-command (concat "exercism fetch " language-name)))
-      (message (exercism-mode/call-shell-command (concat "exercism fetch " (read-from-minibuffer (concat (exercism-mode/call-shell-command "exercism tracks")
-                                                                                                         "\nSelect your programming language: "))))))))
+      (message (exercism-mode/call-shell-command
+                (read-from-minibuffer (exercism-mode/style-given-tracks
+                                       (concat (exercism-mode/call-shell-command "exercism tracks")
+                                               "\nSelect your programming language: "))))))))
 
 (defun exercism-mode ()
   (interactive)
